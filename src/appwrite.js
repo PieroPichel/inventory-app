ebf75763dd8b3b6d2bde0bc9427a3d3f02aed8b8
PieroPichel <piero@pichel.com.br>
2026-02-01 14:23:34 +0000

    Fix Appwrite client chain syntax

--- src/appwrite.js
+++ src/appwrite.js
@@ -1,10 +1,9 @@
 import { Client, Account, Databases, Query, ID } from "appwrite";
 
 const client = new Client()
-	.setEndpoint("https://api.bitswork.net");
-	.setProject("697dc55900296fa9ede8");
+  .setEndpoint("https://api.bitswork.net")
+  .setProject("697dc55900296fa9ede8");
 
 export const account = new Account(client);
 export const databases = new Databases(client);
 export { ID, Query };
-
